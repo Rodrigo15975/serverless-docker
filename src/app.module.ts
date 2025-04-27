f2507b06288454687f0eeb2878a32419fde228dc
rodrigo15975 <javierluis15975@hotmail.com>
2025-04-27 05:50:13 -0500

    build(deps): update package-lock.json, package.json and app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { AuthModule } from './module/auth/auth.module'
 import { TaskModule } from './module/task/task.module'
 import { JwtModule } from '@nestjs/jwt'
 import { BotModule } from './bot/bot.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     JwtModule.register({
       global: true,
       secret: 'secret',
