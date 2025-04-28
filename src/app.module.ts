cd6130c714afd90f3c32babe8ec92030482f5e4a
rodrigo15975 <javierluis15975@hotmail.com>
2025-04-28 00:57:23 -0500

    update app.module.ts, bot.controller.ts, bot.module.ts and bot.service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { PrismaModule } from 'nestjs-prisma'
 import { AuthModule } from './module/auth/auth.module'
 import { TaskModule } from './module/task/task.module'
 import { JwtModule } from '@nestjs/jwt'
-import { BotModule } from './bot/bot.module'
+// import { BotModule } from './bot/bot.module'
 import { ConfigModule } from '@nestjs/config'
 
 @Module({
@@ -19,7 +19,7 @@ import { ConfigModule } from '@nestjs/config'
     PrismaModule.forRoot({ isGlobal: true }),
     AuthModule,
     TaskModule,
-    BotModule,
+    // BotModule,
   ],
   controllers: [],
   providers: [],
