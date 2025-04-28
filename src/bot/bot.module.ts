4804be9bbd5d5d5a7839c5f5c5d64b22f3f9b5f6
rodrigo15975 <javierluis15975@hotmail.com>
2025-04-28 01:34:07 -0500

    update app.module.ts, bot.controller.ts, bot.module.ts and bot.service.ts

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,10 +1,10 @@
-// import { Module } from '@nestjs/common'
-// import { BotService } from './bot.service'
-// import { BotController } from './bot.controller'
+import { Module } from '@nestjs/common'
+import { BotService } from './bot.service'
+import { BotController } from './bot.controller'
 
-// @Module({
-//   imports: [],
-//   controllers: [BotController],
-//   providers: [BotService],
-// })
-// export class BotModule {}
+@Module({
+  imports: [],
+  controllers: [BotController],
+  providers: [BotService],
+})
+export class BotModule {}
--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,27 +1,27 @@
-// import { Injectable, Logger } from '@nestjs/common'
-// import { CreateBotDto } from './dto/create-bot.dto'
-// import { ConfigService } from '@nestjs/config'
-// import { gemini, createAgent, StateData, Agent } from '@inngest/agent-kit'
-// @Injectable()
-// export class BotService {
-//   private readonly GEMINI_API_KEY: string
-//   private readonly AGENT: Agent<StateData>
-//   private readonly logger: Logger = new Logger(BotService.name)
-//   constructor(private readonly configService: ConfigService) {
-//     this.GEMINI_API_KEY = this.configService.getOrThrow('GEMINI_API_KEY')
-//     this.logger.verbose(`GEMINI_API_KEY: ${this.GEMINI_API_KEY}`)
-//     this.AGENT = createAgent({
-//       name: 'bot',
-//       model: gemini({ apiKey: this.GEMINI_API_KEY, model: 'gemini-2.0-flash' }),
-//       system: "You're a bot support multilanguage chatbot",
-//     })
-//   }
+import { Injectable, Logger } from '@nestjs/common'
+import { CreateBotDto } from './dto/create-bot.dto'
+import { ConfigService } from '@nestjs/config'
+import { gemini, createAgent, StateData, Agent } from '@inngest/agent-kit'
+@Injectable()
+export class BotService {
+  private readonly GEMINI_API_KEY: string
+  private readonly AGENT: Agent<StateData>
+  private readonly logger: Logger = new Logger(BotService.name)
+  constructor(private readonly configService: ConfigService) {
+    this.GEMINI_API_KEY = this.configService.getOrThrow('GEMINI_API_KEY')
+    this.logger.verbose(`GEMINI_API_KEY: ${this.GEMINI_API_KEY}`)
+    this.AGENT = createAgent({
+      name: 'bot',
+      model: gemini({ apiKey: this.GEMINI_API_KEY, model: 'gemini-2.0-flash' }),
+      system: "You're a bot support multilanguage chatbot",
+    })
+  }
 
-//   async create(createBotDto: CreateBotDto) {
-//     const { output } = await this.AGENT.run(createBotDto.message)
+  async create(createBotDto: CreateBotDto) {
+    const { output } = await this.AGENT.run(createBotDto.message)
 
-//     this.logger.debug({
-//       output,
-//     })
-//   }
-// }
+    this.logger.debug({
+      output,
+    })
+  }
+}
